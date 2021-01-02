389e9769fe37975ccef8f130f3e1a2cd912a3833
Jon Solow <jssolow+gh@gmail.com>
2021-01-01 23:04:21 -0500

    Add testing endpoints

--- app.py
+++ app.py
@@ -2,9 +2,33 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from scripts.scrape import scrape_group
 
+import os
+import tracemalloc
+import psutil
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/": {"origins": r"*"}})
+process = psutil.Process(os.getpid())
+tracemalloc.start()
+s = None
+
+@app.route('/memory')
+def print_memory():
+    return {'memory': process.memory_info().rss}
+
+
+@app.route("/snapshot")
+def snap():
+    global s
+    if not s:
+        s = tracemalloc.take_snapshot()
+        return "taken snapshot\n"
+    else:
+        lines = []
+        top_stats = tracemalloc.take_snapshot().compare_to(s, 'lineno')
+        for stat in top_stats[:5]:
+            lines.append(str(stat))
+        return "\n".join(lines)
 
 
 @app.route('/api/', methods=['GET'])
