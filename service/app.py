79ea789a67c471371929cb0986180ab75f4c0e16
Jon Solow <jssolow+gh@gmail.com>
2021-01-02 03:56:12 -0500

    Set port from env var

--- service/app.py
+++ service/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from scripts.scrape import scrape_group
+import os
 
 
 app = Flask(__name__)
+port = int(os.getenv("PORT", "5000"))
 cors = CORS(app, resources={r"/api/": {"origins": r"*"}})
 
 
@@ -31,4 +33,4 @@ def index():
 if __name__ == '__main__':
     # Threaded option to enable multiple instances
     # for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, host="0.0.0.0", port=port)
